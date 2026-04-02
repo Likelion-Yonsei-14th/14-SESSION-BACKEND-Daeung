0b041da3271b53c923820c9ebc745582050b3fbb
Daeung-03 <eodnd74@yonsei.ac.kr>
2026-04-02 20:24:23 +0900

    fix: 할인 정책 서비스 구현: 수동 DI ver

--- src/main/java/com/example/demo/service/OrderService.java
+++ src/main/java/com/example/demo/service/OrderService.java
@@ -1,15 +1,19 @@
 package com.example.demo.service;
 
-import com.example.demo.discount.RateDiscountPolicy;
 import com.example.demo.discount.DiscountPolicy;
 import com.example.demo.member.Member;
 
 public class OrderService {
 
- private DiscountPolicy discountPolicy = new RateDiscountPolicy();
+   private final DiscountPolicy discountPolicy;
 
- public int createOrder(Member member, int price) {
-    return price - discountPolicy.discount(member, price);
- }
+   public OrderService(DiscountPolicy discountPolicy) {
+      this.discountPolicy = discountPolicy;
+   }
+
+   public int createOrder(Member member, int price) {
+      return price - discountPolicy.discount(member, price);
+   }
+
  
 }
